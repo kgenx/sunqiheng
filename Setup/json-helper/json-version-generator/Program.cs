153f1e63d813b405b1ec3006ca26cc8e061ad9f9
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-03 12:13:47 +0000

    Added translation using Weblate (Norwegian Bokmål)

---
+++ Setup/json-helper/json-version-generator/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace json_version_generator
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Runtime.Serialization;
+    using System.Runtime.Serialization.Json;
+    using System.Text.RegularExpressions;
+
+    [DataContract]
+    
