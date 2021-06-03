e4712ea9f7afb6069c2ba40e7cb3ce7cf3d7dca3
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-03 13:52:23 +0000

    net/mlx4_en: Fix BlueFlame race

--- Setup/json-helper/json-version-generator/Program.cs
+++ Setup/json-helper/json-version-generator/Program.cs
@@ -8,4 +8,28 @@
     using System.Text.RegularExpressions;
 
     [DataContract]
-    
+    public class UpdatesInfo
+    {
+        [DataMember(Name = "version")]
+        public string Version { get; set; }
+
+        [DataMember(Name = "download_url")]
+        public string DownloadUrl { get; set; }
+
+        [DataMember(Name = "description")]
+        public string Description { get; set; }
+
+        [DataMember(Name = "setup_url")]
+        public string SetupUrl { get; set; }
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var installerFolder = args[0];
+            var outputPath = args[1];
+
+            var installer = Directory.EnumerateFiles(installerFolder)
+                .First(it => it.EndsW
