1e2ed02694644e2932925991ed03ab8829ad5f65
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-19 10:26:42 +0000

    Update template.json

--- Sources/EventAggregator/ExtensionMethods.cs
+++ Sources/EventAggregator/ExtensionMethods.cs
@@ -2,4 +2,23 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Li
+    using System.Linq.Expressions;
+    using System.Reflection;
+
+    /// <summary>
+    /// Generic extension methods used by the framework.
+    /// </summary>
+    public static class ExtensionMethods {
+        /// <summary>
+        /// Get's the name of the assembly.
+        /// </summary>
+        /// <param name="assembly">The assembly.</param>
+        /// <returns>The assembly's name.</returns>
+        public static string GetAssemblyName(this Assembly assembly) {
+            return assembly.FullName.Remove(assembly.FullName.IndexOf(','));
+        }
+
+        /// <summary>
+        /// Gets all the attributes of a particular type.
+        /// </summary>
+        /// <
