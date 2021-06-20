66b7e895f2a40e780382011168ac10360aff4266
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-20 10:08:45 +0000

    Small bug

--- Sources/EventAggregator/ExtensionMethods.cs
+++ Sources/EventAggregator/ExtensionMethods.cs
@@ -33,4 +33,17 @@
         /// Applies the action to each element in the list.
         /// </summary>
         /// <typeparam name="T">The enumerable item's type.</typeparam>
-        /// <par
+        /// <param name="enumerable">The elements to enumerate.</param>
+        /// <param name="action">The action to apply to each item in the list.</param>
+        public static void Apply<T>(this IEnumerable<T> enumerable, Action<T> action) {
+            foreach(var item in enumerable) {
+                action(item);
+            }
+        }
+
+        /// <summary>
+        /// Converts an expression into a <see cref="MemberInfo"/>.
+        /// </summary>
+        /// <param name="expression">The expression to convert.</param>
+        /// <returns>The member info.</returns>
+        public static MemberInfo GetMemberInfo(this Expre
