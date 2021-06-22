c54c4e3e9f245e6213bd1f3b0517232161e6f3f6
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-22 06:48:43 +0000

    strategy

--- Sources/EventAggregator/ExtensionMethods.cs
+++ Sources/EventAggregator/ExtensionMethods.cs
@@ -46,4 +46,19 @@
         /// </summary>
         /// <param name="expression">The expression to convert.</param>
         /// <returns>The member info.</returns>
-        public static MemberInfo GetMemberInfo(this Expre
+        public static MemberInfo GetMemberInfo(this Expression expression) {
+            var lambda = (LambdaExpression)expression;
+
+            MemberExpression memberExpression;
+            if (lambda.Body is UnaryExpression) {
+                var unaryExpression = (UnaryExpression)lambda.Body;
+                memberExpression = (MemberExpression)unaryExpression.Operand;
+            }
+            else {
+                memberExpression = (MemberExpression)lambda.Body;
+            }
+
+            return memberExpression.Member;
+        }
+    }
+}
---
+++ Sources/EventAggregator/IHandle.cs
@@ -0,0 +1,4 @@
+namespace Infrastructure.Messaging
+{
+    /// <summary>
+    ///  
