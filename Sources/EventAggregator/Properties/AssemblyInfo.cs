705d423138c446b6ec66fac60eebf374bb937cbe
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-25 09:06:43 +0000

    Tidy code.

--- Sources/EventAggregator/Properties/AssemblyInfo.cs
+++ Sources/EventAggregator/Properties/AssemblyInfo.cs
@@ -1,3 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using Sy
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EventAggregator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EventAggregator")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type 
