1dbc5f8bb401d4f45c0ecce953a7d597040e598b
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-25 09:56:01 +0000

    Another

--- Sources/EventAggregator/Properties/AssemblyInfo.cs
+++ Sources/EventAggregator/Properties/AssemblyInfo.cs
@@ -15,4 +15,21 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("224b2fc5-5d04-443e-9bb6-89e8afd04a8e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("
