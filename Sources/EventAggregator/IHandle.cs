b912432ea1cba60a48da58d7e445656a4ffb2371
kgenx <112470300+kgenx@users.noreply.github.com>
2021-06-25 08:02:46 +0000

    langues (spip)

--- Sources/EventAggregator/IHandle.cs
+++ Sources/EventAggregator/IHandle.cs
@@ -1,4 +1,20 @@
 namespace Infrastructure.Messaging
 {
     /// <summary>
-    ///  
+    ///   A marker interface for classes that subscribe to messages.
+    /// </summary>
+    public interface IHandle { }
+
+    /// <summary>
+    ///   Denotes a class which can handle a particular type of message.
+    /// </summary>
+    /// <typeparam name = "TMessage">The type of message to handle.</typeparam>
+    public interface IHandle<TMessage> : IHandle
+    {  //don't use contravariance here
+        /// <summary>
+        ///   Handles the message.
+        /// </summary>
+        /// <param name = "message">The message.</param>
+        void Handle(TMessage message);
+    }
+}
