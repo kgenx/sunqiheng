a821e7ac7d4490378cd27f8fd0449b819a1be6c9
kgenx <112470300+kgenx@users.noreply.github.com>
2021-07-10 10:00:58 +0000

    Version bump;

---
+++ Sources/Infrastructure/IEncryptor.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure
+{
+    public interface IEncryptor
+    {
+        byte[] Encrypt(byte[] data);
+        byte[] Decrypt(byte[] data);
+    }
+}
---
+++ Sources/Infrastructure/IStorageProvider.cs
@@ -0,0 +1,8 @@
+﻿namespace Infrastructure
+{
+    public interface IStorageProvider
+    {
+        string Load(string path = null);
+        void Save(string data, string path = null);
+    }
+}
