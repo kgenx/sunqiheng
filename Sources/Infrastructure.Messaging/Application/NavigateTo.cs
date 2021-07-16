0672710cdf82b2e03921ae3c8109d4b400f80fed
kgenx <112470300+kgenx@users.noreply.github.com>
2021-07-16 14:15:22 +0000

    Update Modern Agriculture.md

--- Sources/Infrastructure.Messaging/Application/NavigateTo.cs
+++ Sources/Infrastructure.Messaging/Application/NavigateTo.cs
@@ -4,4 +4,18 @@
 
     public class NavigateTo
     {
-       
+        public NavigateTo(Type type)
+        {
+            this.Type = type;
+        }
+
+        public NavigateTo(Type type, object @params)
+        {
+            this.Type = type;
+            this.Params = @params;
+        }
+
+        public Type Type { get; }
+        public object Params { get; }
+    }
+}
---
+++ Sources/Infrastructure.Messaging/Application/ProblemsSigningIn.cs
@@ -0,0 +1,11 @@
+namespace Infrastructure.Messaging.Application
+{
+    public class ProblemsSigningIn
+    {
+        public ProblemsSigningIn(string email)
+        {
+            this.Email = email;
+        }
+
+        public ProblemsSigningIn()
+     
