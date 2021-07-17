e0c1897135d3084be7c45d19a06027a5131e800a
kgenx <112470300+kgenx@users.noreply.github.com>
2021-07-17 08:17:10 +0000

    warnings removed

--- Sources/Infrastructure.Messaging/EventAggregator.cs
+++ Sources/Infrastructure.Messaging/EventAggregator.cs
@@ -8,4 +8,20 @@
     ///   Enables loosely-coupled publication of and subscription to events.
     /// </summary>
     public class EventAggregator : IEventAggregator {
-   
+        readonly List<Handler> handlers = new List<Handler>();
+
+        /// <summary>
+        ///   The default thread marshaller used for publication;
+        /// </summary>
+        public static Action<System.Action> DefaultPublicationThreadMarshaller = action => action();
+
+        /// <summary>
+        /// Processing of handler results on publication thread.
+        /// </summary>
+        public static Action<object, object> HandlerResultProcessing = (target, result) => { };
+
+        /// <summary>
+        ///   Initializes a new instance of the <see cref = "EventAggregator" /> class.
+        /// </summary>
+        public EventAggregator() {
+            this.Public
