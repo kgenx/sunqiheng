354203817e6d67caaa793dc348dcdac122366ed0
kgenx <112470300+kgenx@users.noreply.github.com>
2021-08-09 06:59:48 +0000

    review code

--- Sources/Infrastructure.Messaging/IHandle.cs
+++ Sources/Infrastructure.Messaging/IHandle.cs
@@ -6,4 +6,15 @@ namespace Infrastructure.Messaging
     public interface IHandle { }
 
     /// <summary>
-    //
+    ///   Denotes a class which can handle a particular type of message.
+    /// </summary>
+    /// <typeparam name = "TMessage">The type of message to handle.</typeparam>
+    public interface IHandle<TMessage> : IHandle
+    {  //don't use contravariance here
+        /// <summary>
+        ///   Handles the message.
+        /// </summary>
+        /// <param name = "message">The message.</param>
+        void Handle(TMessage message);
+    }
+}
