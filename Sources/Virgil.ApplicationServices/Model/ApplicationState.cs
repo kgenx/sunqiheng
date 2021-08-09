934c74eed8b4cf10ce5ac1eea23cd8b62182a1c9
kgenx <112470300+kgenx@users.noreply.github.com>
2021-08-09 09:49:38 +0000

    capitalisation

--- Sources/Virgil.ApplicationServices/Model/ApplicationState.cs
+++ Sources/Virgil.ApplicationServices/Model/ApplicationState.cs
@@ -1,3 +1,25 @@
 namespace Virgil.Disk.Model
 {
-  
+    using System;
+    using System.IO;
+    using System.Text;
+    using Infrastructure.Messaging;
+    using LocalStorage;
+    using Messages;
+    using Newtonsoft.Json;
+    using Ookii.Dialogs.Wpf;
+    using SDK.Domain;
+
+    public class ApplicationState : IHandle<CardLoaded>
+    {
+        private readonly IEventAggregator aggregator;
+        private readonly IStorageProvider storageProvider;
+
+        public ApplicationState(IEventAggregator aggregator, IStorageProvider storageProvider)
+        {
+            this.aggregator = aggregator;
+            this.storageProvider = storageProvider;
+            aggregator.Subscribe(this);
+        }
+
+        public PersonalCard CurrentCa
