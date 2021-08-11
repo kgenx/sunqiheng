cf4bffd6f6c300b886c0b7bfad8e6ac2659ea2e0
kgenx <112470300+kgenx@users.noreply.github.com>
2021-08-11 09:58:09 +0000

    TESTE

--- Sources/Virgil.ApplicationServices/Model/FolderSettings.cs
+++ Sources/Virgil.ApplicationServices/Model/FolderSettings.cs
@@ -2,4 +2,31 @@
 {
     using System;
     using System.Collections.Generic;
-    using Sy
+    using System.Linq;
+    using Dropbox.Api;
+    using Infrastructure;
+    using Newtonsoft.Json;
+
+    public class DropboxCredentials
+    {
+        public DropboxCredentials()
+        {
+        }
+
+        public DropboxCredentials(OAuth2Response oauth)
+        {
+            this.AccessToken = oauth.AccessToken;
+            this.UserId = oauth.Uid;
+        }
+
+        [JsonProperty]
+        public string AccessToken { get; set; }
+
+        [JsonProperty]
+        public string UserId { get; set; }
+
+        public bool IsEmpty()
+        {
+            return string.IsNullOrEmpty(this.AccessToken) && string.IsNullOrWhiteSpace(this.UserId);
+        }
+   
