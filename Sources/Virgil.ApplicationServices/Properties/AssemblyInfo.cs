fa6b47a65f53f90a25cccb8788740e1a5e4b25be
kgenx <112470300+kgenx@users.noreply.github.com>
2021-08-18 09:17:27 +0000

    some clean

--- Sources/Virgil.ApplicationServices/Properties/AssemblyInfo.cs
+++ Sources/Virgil.ApplicationServices/Properties/AssemblyInfo.cs
@@ -7,4 +7,23 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Virgil.ApplicationServices")]
-[assembly: AssemblyDescription
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Virgil.ApplicationServices")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyV
