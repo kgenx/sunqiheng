bafc9a4c9a79498c3382c57e1ff6baea62efc55a
kgenx <112470300+kgenx@users.noreply.github.com>
2021-08-24 18:29:34 +0000

    merging conflict

--- Sources/Virgil.CLI.Common/Options/ConfigureOptions.cs
+++ Sources/Virgil.CLI.Common/Options/ConfigureOptions.cs
@@ -1,1 +1,16 @@
-﻿namespace
+﻿namespace Virgil.CLI.Common.Options
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+    using CommandLine;
+    using CommandLine.Text;
+
+    [Verb("config", HelpText = "Configures Virgil Sync to use specific Virgil card and dropbox account.")]
+    public class ConfigureOptions
+    {
+        [Option('v', "virgil-card", Required = true, HelpText = "Path to the virgil card file created from CLI.")]
+        public string VirgilCardPath { get; set; }
+
+        [Option('k', "private-key", Required = true, HelpText = "Path to the private key of the specified virgil card.")]
+        public string PrivateKeyPath { 
