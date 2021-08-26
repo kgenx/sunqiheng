7ead84d53a5c78ae39a5f1132ac0e11516baf658
kgenx <112470300+kgenx@users.noreply.github.com>
2021-08-26 22:43:30 +0000

    Added example to readme

--- Sources/Virgil.CLI.Common/Properties/AssemblyInfo.cs
+++ Sources/Virgil.CLI.Common/Properties/AssemblyInfo.cs
@@ -1,3 +1,18 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Virgil.CLI.Common")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Virgil.CLI.Common")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access
