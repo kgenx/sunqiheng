bdafdb4da43ed5fc39105f8c1142273cab67dbd3
kgenx <112470300+kgenx@users.noreply.github.com>
2021-08-27 07:29:21 +0000

    [maven-release-plugin] prepare release v1.2.2

--- Sources/Virgil.CLI.Common/Properties/AssemblyInfo.cs
+++ Sources/Virgil.CLI.Common/Properties/AssemblyInfo.cs
@@ -15,4 +15,22 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39771eea-b6b5-4a12-9cec-6f372defa973")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVers
