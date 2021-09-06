7c8e2da484ea59fb7b116fea45c820ba90e75d5a
kgenx <112470300+kgenx@users.noreply.github.com>
2021-09-06 09:59:13 +0000

    SCSI: zfcp: remove invalid reference to list iterator variable

--- Sources/Virgil.Disk/ApiConfig.cs
+++ Sources/Virgil.Disk/ApiConfig.cs
@@ -2,4 +2,8 @@
 {
     public class ApiConfig
     {
-    
+        public const string DropboxClientId    = "";
+        public const string VirgilToken        = "";
+        public const string VirgilTokenStaging = "";
+    }
+}
