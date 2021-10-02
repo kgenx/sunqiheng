1a01c83078b299def0f99faf74905eb54d7764c9
kgenx <112470300+kgenx@users.noreply.github.com>
2021-10-02 13:49:30 +0000

    \tFix typo.

--- Sources/Virgil.Disk/App.xaml.cs
+++ Sources/Virgil.Disk/App.xaml.cs
@@ -22,3 +22,86 @@
     public partial class App : Application
     {
         public Bootstrapper Bootstrapper { get; private set; }
+        
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            this.DispatcherUnhandledException += this.OnDispatcherUnhandledException;
+
+            var virgilHub = ServiceHub.Create(ServiceHubConfig.UseAccessToken(ApiConfig.VirgilToken));
+
+            Virgil.SDK.Domain.ServiceLocator.Setup(virgilHub);
+
+            var updater = new Updater();
+            updater.Start();
+
+            this.Bootstrapper = new Bootstrapper();
+            this.Bootstrapper.Initialize();
+
+            this.AppState = this.Bootstrapper.IoC.Get<ApplicationState>();
+            this.AppState.Restore();
+            
+            this.FolderSettings = this.Bootstrapper.IoC.Get<FolderSettingsStorage>();
+            this.MainWindow = new TrayWindow
+            {
+                DataContext = this.Bootstrapper.IoC.Get<OperationStatusViewModel>()
+            };
+
+            ExceptionNotifier.Current.OnDropboxSessionExpired(() =>
+            {
+                this.Dispatcher.Invoke(() =>
+                {
+                    this.Bootstrapper.IoC.Get<IEventAggregator>().Publish(new DropboxSessionExpired());
+                });
+            });
+
+            this.ShowUI();
+        }
+
+        public ApplicationState AppState { get; private set; }
+        public FolderSettingsStorage FolderSettings { get; private set; }
+
+        public void ShowUI()
+        {
+            ContainerWindow container = ContainerWindow.CurrentInstance ?? new ContainerWindow
+            {
+                DataContext = ServiceLocator.Resolve<ContainerViewModel>()
+            };
+            //container.SetScaling(Virgil.Disk.Windows.GetRawDpi());
+            container.Topmost = true;
+            container.Topmost = false;
+            container.Show();
+            container.PositionWindowOnScreen();
+            container.Activate();
+        }
+
+        public void ShowDecryptedDirectory()
+        {
+            try
+            {
+                var targetFolderPath = this.FolderSettings.FolderSettings.SourceFolder.FolderPath ?? "";
+                if (Directory.Exists(targetFolderPath))
+                {
+                    Process.Start(targetFolderPath);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Folder settings is null");
+            }
+        }
+
+        public void Logout()
+        {
+            this.AppState.Logout();
+        }
+
+        private void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs args)
+        {
+            args.Handled = true;
+            MessageBox.Show(args.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+    }
+}
---
+++ Sources/Virgil.Disk/Bootstrapper.cs
@@ -0,0 +1,21 @@
+﻿namespace Virgil.Sync
+{
+    using FolderLink.Facade;
+    using Infrastructure;
+    using Infrastructure.Messaging;
+    using LocalStorage;
+    using Ninject;
+    using Ninject.Parameters;
+    using ViewModels;
+
+    public class Bootstrapper
+    {
+        public StandardKernel IoC;
+
+        public void Initialize()
+        {
+            this.IoC = new StandardKernel();
+
+            this.IoC.Bind<ApplicationState>().ToSelf().InSingletonScope();
+            this.IoC.Bind<FolderSettingsStorage>().ToSelf().InSingletonScope();
+            this.IoC.Bind<IEventAggr
