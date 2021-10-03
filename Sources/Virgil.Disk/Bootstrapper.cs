9df0571ee42b4b6af081197623ed323bac146945
kgenx <112470300+kgenx@users.noreply.github.com>
2021-10-03 13:33:02 +0000

    some ui changes

--- Sources/Virgil.Disk/Bootstrapper.cs
+++ Sources/Virgil.Disk/Bootstrapper.cs
@@ -18,4 +18,15 @@
 
             this.IoC.Bind<ApplicationState>().ToSelf().InSingletonScope();
             this.IoC.Bind<FolderSettingsStorage>().ToSelf().InSingletonScope();
-            this.IoC.Bind<IEventAggr
+            this.IoC.Bind<IEventAggregator>().To<EventAggregator>().InSingletonScope();
+            this.IoC.Bind<FolderLinkFacade>().ToSelf().InSingletonScope();
+
+            this.IoC.Bind<IStorageProvider>().To<IsolatedStorageProvider>().InSingletonScope();
+            this.IoC.Bind<IEncryptor>().To<WindowsPerUserEncryptor>().InSingletonScope();
+
+            this.IoC.Bind<ConfirmationCodeViewModel>().ToSelf().InSingletonScope();
+
+            this.IoC.Bind<IRegenerateKeypairModel>()
+                .To<CreateAccountViewModel>()
+                .InSingletonScope()
+                .WithParameter(new ConstructorArgument("state", CreateAccountViewModel.State.Regenerate
