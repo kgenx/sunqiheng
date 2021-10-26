ecf4d90853b711aa65ba4d898f10e53cb7d77976
kgenx <112470300+kgenx@users.noreply.github.com>
2021-10-26 17:01:26 +0000

    add version command

--- Sources/Virgil.Disk/ServiceLocator.cs
+++ Sources/Virgil.Disk/ServiceLocator.cs
@@ -1,3 +1,26 @@
 ﻿namespace Infrastructure
 {
-   
+    using System;
+    using Ninject;
+    using Ninject.Parameters;
+
+    public class ServiceLocator
+    {
+        private static IKernel container;
+
+        public static TService Resolve<TService>()
+        {
+            return container.Get<TService>();
+        }
+
+        public static object Resolve(Type type)
+        {
+            return container.Get(type);
+        }
+
+        public static void SetContainer(IKernel kernel)
+        {
+            container = kernel;
+        }
+    }
+}
