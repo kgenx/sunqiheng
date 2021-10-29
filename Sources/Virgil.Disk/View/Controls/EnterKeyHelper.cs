4ab9003f17e5c2009d4c0a27341cd2b0aac219a4
kgenx <112470300+kgenx@users.noreply.github.com>
2021-10-29 21:58:37 +0000

    Compilation fixed

--- Sources/Virgil.Disk/View/Controls/EnterKeyHelper.cs
+++ Sources/Virgil.Disk/View/Controls/EnterKeyHelper.cs
@@ -8,4 +8,22 @@
     {
         public static ICommand GetEnterKeyCommand(DependencyObject target)
         {
-            return (ICommand)target.G
+            return (ICommand)target.GetValue(EnterKeyCommandProperty);
+        }
+
+        public static void SetEnterKeyCommand(DependencyObject target, ICommand value)
+        {
+            target.SetValue(EnterKeyCommandProperty, value);
+        }
+
+        public static readonly DependencyProperty EnterKeyCommandProperty =
+            DependencyProperty.RegisterAttached(
+                "EnterKeyCommand",
+                typeof(ICommand),
+                typeof(EnterKeyHelper),
+                new PropertyMetadata(null, OnEnterKeyCommandChanged));
+
+
+        public static object GetEnterKeyCommandParam(DependencyObject target)
+        {
+            return (object)target.GetValue(EnterKeyComma
