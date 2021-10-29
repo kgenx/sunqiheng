82002d0bf3a47e4f9f1d973ca2befa5bb2bf6082
kgenx <112470300+kgenx@users.noreply.github.com>
2021-10-29 12:07:22 +0000

    Add a note

---
+++ Sources/Virgil.Disk/View/ConfirmationCodeView.xaml.cs
@@ -0,0 +1,16 @@
+
+﻿namespace Virgil.Sync.View
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for ConfirmationCodeView.xaml
+    /// </summary>
+    public partial class ConfirmationCodeView : UserControl
+    {
+        public ConfirmationCodeView()
+        {
+            InitializeComponent();
+        }
+    }
+}
