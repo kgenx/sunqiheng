9e86b844c6c1a258338b80264e3362ba72d4eb0d
kgenx <112470300+kgenx@users.noreply.github.com>
2021-10-29 12:08:13 +0000

    add address

---
+++ Sources/Virgil.Disk/View/ContainerWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace Virgil.Sync.View
+{
+    using System;
+    using System.Windows;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
+
+    /// <summary>
+    /// Interaction logic for Container.xaml
+    /// </summary>
+    public partial class ContainerWindow : Window
+    {
+        private readonly double AspectRatio;
+
+        public static ContainerWindow CurrentInstance { get; private set; } = null;
+
+        public ContainerWindow() : base()
+        {
+            InitializeComponent();
+            this.MouseDown += this.OnMouseDown
