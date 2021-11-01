3cfb47080e37d87d1d03fd8591053cf016cc6363
kgenx <112470300+kgenx@users.noreply.github.com>
2021-11-01 19:29:32 +0000

    fix index.html

---
+++ Sources/Virgil.Disk/View/Controls/SelectKeyTextBox.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace Virgil.Sync.View.Controls
+{
+    using System.Windows.Forms;
+    using Infrastructure.Mvvm;
+    using Ookii.Dialogs.Wpf;
+    using UserControl = System.Windows.Controls.UserControl;
+
+    /// <summary>
+    /// Interaction logic for SelectKeyTextBox.xaml
+    /// </summary>
+    public partial class SelectKeyTextBox : UserControl
+    {
+        public SelectKeyTextBox()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly DependencyProperty SelectedPathProperty = DependencyProperty.Register(
+            
