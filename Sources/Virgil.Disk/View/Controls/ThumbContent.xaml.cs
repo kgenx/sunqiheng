e99605c12b81e4d51ed502661556ba433e16ff10
kgenx <112470300+kgenx@users.noreply.github.com>
2021-11-07 12:58:11 +0000

    update keys

--- Sources/Virgil.Disk/View/Controls/ThumbContent.xaml.cs
+++ Sources/Virgil.Disk/View/Controls/ThumbContent.xaml.cs
@@ -2,4 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Syst
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Virgil.Sync.View.Controls
+{
+    /// <summary>
+    /// Interaction logic for ThumbContent.xaml
+    /// </summary>
+    public partial class ThumbContent : UserControl
+    {
+        public ThumbContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
