dc22b42ee50fbd0274c9f26e41ed6f9a3b936609
kgenx <112470300+kgenx@users.noreply.github.com>
2021-11-10 11:28:53 +0000

    add price format

---
+++ Sources/Virgil.Disk/View/Controls/TooltipControl.xaml.cs
@@ -0,0 +1,16 @@
+
+﻿namespace Virgil.Sync.View.Controls
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for TooltipControl.xaml
+    /// </summary>
+    public partial class TooltipControl : UserControl
+    {
+        public TooltipControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
