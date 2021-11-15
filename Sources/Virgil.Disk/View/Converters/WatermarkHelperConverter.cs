829195d9476fb5f762f26735a3eaf3c7e5811019
kgenx <112470300+kgenx@users.noreply.github.com>
2021-11-15 16:23:59 +0000

    Updated strings

--- Sources/Virgil.Disk/View/Converters/WatermarkHelperConverter.cs
+++ Sources/Virgil.Disk/View/Converters/WatermarkHelperConverter.cs
@@ -12,4 +12,26 @@ namespace Virgil.Sync.View.Converters
         {
             if (values != null)
             {
-                var isFocused = values.OfTyp
+                var isFocused = values.OfType<bool>().Any(b => b);
+                if (isFocused)
+                {
+                    return Visibility.Collapsed;
+                }
+
+                var isEmpty = string.IsNullOrEmpty(values.OfType<string>().FirstOrDefault());
+
+                if (isEmpty)
+                {
+                    return Visibility.Visible;
+                }
+
+                return Visibility.Collapsed;
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+    
