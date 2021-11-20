4a306014e96c1e618484375e4450acef15b59909
kgenx <112470300+kgenx@users.noreply.github.com>
2021-11-20 09:45:09 +0000

    Update quickstart

---
+++ Sources/Virgil.Disk/View/FolderSettingsDropBoxView.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Virgil.Sync.View
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for FolderSettings.xaml
+    /// </summary>
+    public partial class FolderSettingsDropBoxView : UserControl
+    {
+        public FolderSettingsDropBoxView()
+        {
+            this.InitializeComponent();
+        }
+
+        
+    }
+}
