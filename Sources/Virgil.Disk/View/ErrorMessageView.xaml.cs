5ead8269ce4aa5fe9942bafc7a2a57941224530e
kgenx <112470300+kgenx@users.noreply.github.com>
2021-11-20 07:07:57 +0000

    Rename again

---
+++ Sources/Virgil.Disk/View/ErrorMessageView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Virgil.Sync.View
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for DropBoxView.xaml
+    /// </summary>
+    public partial class ErrorMessageView : UserControl
+    {
+        public ErrorMessageView()
+        {
+            InitializeComponent();
+        }
+    }
+}
