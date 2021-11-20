0dfeb06f51adb69a2f5c9548de404dd7f37a80d4
kgenx <112470300+kgenx@users.noreply.github.com>
2021-11-20 16:07:53 +0000

    1.1.28

---
+++ Sources/Virgil.Disk/View/FolderSettingsView.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace Virgil.Sync.View
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using ViewModels;
+
+    /// <summary>
+    /// Interaction logic for FolderSettings.xaml
+    /// </summary>
+    public partial class FolderSettingsView : UserControl
+    {
+        public FolderSettingsView()
+        {
+            this.InitializeComponent();
+
+            this.UpdateAnimation(false);
+        }
+
+        public static readonly DependencyProperty SettingsStageProperty = DependencyProperty.Re
