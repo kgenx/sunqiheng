ef27ebc308169c23977076e49824d8f20372a4b0
kgenx <112470300+kgenx@users.noreply.github.com>
2021-12-06 10:57:01 +0000

    Fix homepage URL

---
+++ Sources/Virgil.Disk/View/OperationStatusView.xaml.cs
@@ -0,0 +1,16 @@
+
+﻿namespace Virgil.Sync.View
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for OperationStatusView.xaml
+    /// </summary>
+    public partial class OperationStatusView : UserControl
+    {
+        public OperationStatusView()
+        {
+            InitializeComponent();
+        }
+    }
+}
