cb70ae6128d91d39279f316e4555fdf582026fe5
kgenx <112470300+kgenx@users.noreply.github.com>
2021-12-10 17:07:07 +0000

    Remove extra blank lines

---
+++ Sources/Virgil.Disk/View/SignInView.xaml.cs
@@ -0,0 +1,16 @@
+
+﻿namespace Virgil.Sync.View
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for SignInView.xaml
+    /// </summary>
+    public partial class SignInView : UserControl
+    {
+        public SignInView()
+        {
+            InitializeComponent();
+        }
+    }
+}
