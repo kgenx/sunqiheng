e850287e26bc1667b848a63bbba0c3bf76c516e4
kgenx <112470300+kgenx@users.noreply.github.com>
2021-12-11 06:17:45 +0000

    Add integration test

--- Sources/Virgil.Disk/View/TrayWindow.xaml.cs
+++ Sources/Virgil.Disk/View/TrayWindow.xaml.cs
@@ -2,4 +2,26 @@
 {
     using System;
     using System.IO;
-    using System.T
+    using System.Threading.Tasks;
+    using System.Windows;
+    using Hardcodet.Wpf.TaskbarNotification;
+    using Infrastructure;
+    using Infrastructure.Messaging;
+    using Infrastructure.Messaging.Application;
+    using Ookii.Dialogs.Wpf;
+    using Application = System.Windows.Application;
+
+    /// <summary>
+    /// Interaction logic for TrayWindow.xaml
+    /// </summary>
+    public partial class TrayWindow : Window,
+        IHandle<ErrorMessage>,
+        IHandle<CardLoaded>,
+        IHandle<Logout>,
+        IHandle<FolderSettingsChanged>
+    {
+        public TrayWindow()
+        {
+            this.InitializeComponent();
+
+      
