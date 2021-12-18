abb5a1d38ec68fa97e374e18d25682e1d539b7eb
kgenx <112470300+kgenx@users.noreply.github.com>
2021-12-18 10:35:25 +0000

    Add `.eslintrc`

--- Sources/Virgil.Disk/View/WebBrowserUtility.cs
+++ Sources/Virgil.Disk/View/WebBrowserUtility.cs
@@ -2,4 +2,19 @@ namespace Virgil.Sync.View
 {
     using System;
     using System.Globalization;
-    using Sys
+    using System.Windows;
+    using System.Windows.Controls;
+
+    public static class WebBrowserUtility
+    {
+        public static readonly DependencyProperty BindableSourceProperty =
+            DependencyProperty.RegisterAttached("BindableSource", typeof(string), typeof(WebBrowserUtility), new UIPropertyMetadata(null, BindableSourcePropertyChanged));
+
+        public static string GetBindableSource(DependencyObject obj)
+        {
+            return (string)obj.GetValue(BindableSourceProperty);
+        }
+
+        public static void SetBindableSource(DependencyObject obj, string value)
+        {
+            obj.SetValue(Bind
