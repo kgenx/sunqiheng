8237ea02312f5aa1c3f018ebcfc9d449a129ad89
kgenx <112470300+kgenx@users.noreply.github.com>
2021-12-18 13:02:49 +0000

    Added events

--- Sources/Virgil.Disk/ViewModels/CreateAccountViewModel.cs
+++ Sources/Virgil.Disk/ViewModels/CreateAccountViewModel.cs
@@ -3,4 +3,27 @@
     using System;
     using System.Windows.Input;
     using Infrastructure.Messaging;
-    using Infrastructure
+    using Infrastructure.Messaging.Application;
+    using Infrastructure.Mvvm;
+    using Operations;
+    using SDK.Exceptions;
+    using Sync.Messages;
+
+    public interface ICreateNewAccountModel
+    {
+        
+    }
+
+    public interface IRegenerateKeypairModel
+    {
+        string Login { get; set; }
+    }
+
+    public class CreateAccountViewModel : ViewModel, ICreateNewAccountModel, IRegenerateKeypairModel
+    {
+        private readonly IEventAggregator aggregator;
+        private readonly State state;
+        private string confirmPassword;
+        private string login;
+        private string password;
+        private bool isUploadPrivateKey
