0a7a777fb243ab491b4db3498f66c2b27708c9b5
kgenx <112470300+kgenx@users.noreply.github.com>
2022-01-01 21:08:29 +0000

    1st draft

--- Sources/Virgil.Disk/ViewModels/ErrorMessageViewModel.cs
+++ Sources/Virgil.Disk/ViewModels/ErrorMessageViewModel.cs
@@ -18,4 +18,27 @@
 
             this.OkCommand = new RelayCommand(() =>
             {
-                this.eventAggregator
+                this.eventAggregator.Publish(new NavigateBack());
+            });
+        }
+
+        public void InitiErrorMessageFor(DropboxSessionExpired sessionExpired)
+        {
+            this.ErrorLarge = "Dropbox error";
+            this.ErrorDetails = "Dropbox session timed out, please refresh the session by signing in again.";
+        }
+
+        public ICommand OkCommand { get; }
+
+        public string ErrorLarge
+        {
+            get { return this.errorLarge; }
+            set
+            {
+                if (value == this.errorLarge) return;
+                this.errorLarge = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        publ
