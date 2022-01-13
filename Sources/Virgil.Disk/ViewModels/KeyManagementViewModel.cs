5675e187fe7fe466a91446313a239fa36214d100
kgenx <112470300+kgenx@users.noreply.github.com>
2022-01-13 08:51:01 +0000

    Update SPLU-Beta.js

--- Sources/Virgil.Disk/ViewModels/KeyManagementViewModel.cs
+++ Sources/Virgil.Disk/ViewModels/KeyManagementViewModel.cs
@@ -12,4 +12,23 @@ namespace Virgil.Sync.ViewModels
     using Infrastructure.Mvvm;
     using Newtonsoft.Json;
     using Ookii.Dialogs.Wpf;
-    using SDK.Domai
+    using SDK.Domain;
+    using SDK.Domain.Exceptions;
+
+    public class KeyManagementViewModel : ViewModel
+    {
+        private readonly IEventAggregator aggregator;
+        private string selectedPath;
+        private string password;
+        private VirgilCardDto selectedCard;
+        private bool isMultipleKeys;
+        private bool isShowKeyNotFoundMessage;
+
+        public KeyManagementViewModel(IEventAggregator aggregator)
+        {
+            this.Password = "";
+            this.aggregator = aggregator;
+
+            this.ReturnToSignInCommand = new RelayCommand(() =>
+            {
+                this.aggregator.Publish(new NavigateTo(typeof(SignInVi
