f07222044a55b5534f5cc370205e95033b173079
kgenx <112470300+kgenx@users.noreply.github.com>
2022-01-17 06:18:14 +0000

    less verbosity

--- Sources/Virgil.Disk/ViewModels/OperationStatusViewModel.cs
+++ Sources/Virgil.Disk/ViewModels/OperationStatusViewModel.cs
@@ -7,4 +7,20 @@
     using FolderLink.Dropbox.Messages;
     using Infrastructure.Messaging;
 
-    
+    public class OperationStatusViewModel : ViewModel, IHandle<DropBoxLinkChanged>
+    {
+        private ObservableCollection<Operation> operations;
+        private ObservableCollection<OperationViewModel> operationViewModels;
+
+        public ObservableCollection<OperationViewModel> Operations
+        {
+            get { return this.operationViewModels; }
+            private set
+            {
+                if (Equals(value, this.operationViewModels)) return;
+                this.operationViewModels = value;
+                this.RaisePropertyChanged();
+            }
+        }
+
+        public OperationStatusViewModel(I
