0e2198258251e760bfc8b8ed43aacc6ca1cc2ba6
kgenx <112470300+kgenx@users.noreply.github.com>
2022-01-22 10:12:34 +0000

    fix prev commit

--- Sources/Virgil.Disk/ViewModels/Operations/CreateAccountOperation.cs
+++ Sources/Virgil.Disk/ViewModels/Operations/CreateAccountOperation.cs
@@ -4,4 +4,22 @@ namespace Virgil.Sync.ViewModels.Operations
     using System.Threading.Tasks;
     using Infrastructure.Messaging;
     using Infrastructure.Messaging.Application;
-    usin
+    using SDK.Domain;
+    using SDK.Exceptions;
+
+    public class CreateAccountOperation : IConfirmationRequiredOperation
+    {
+        private readonly IEventAggregator eventAggregator;
+        private readonly bool usePassword;
+        private readonly bool uploadPrivateKey;
+
+        private string email;
+        private string password;
+
+        private IdentityTokenRequest request;
+
+        public CreateAccountOperation(IEventAggregator eventAggregator, bool usePassword, bool uploadPrivateKey)
+        {
+            this.eventAggregator = eventAggregator;
+            this.usePassword = usePassword;
+            this.uploadPrivateKey =
