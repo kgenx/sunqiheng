8a530fc33ae9623e049235b23e4ca39fea010e5d
kgenx <112470300+kgenx@users.noreply.github.com>
2022-01-29 20:03:51 +0000

    builder

--- Sources/Virgil.Disk/ViewModels/Operations/RegenerateKeyPairOperation.cs
+++ Sources/Virgil.Disk/ViewModels/Operations/RegenerateKeyPairOperation.cs
@@ -1,3 +1,21 @@
 namespace Virgil.Sync.ViewModels.Operations
 {
-    using 
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Infrastructure.Messaging;
+    using Infrastructure.Messaging.Application;
+    using SDK.Domain;
+    using SDK.Exceptions;
+
+    public class RegenerateKeyPairOperation : IConfirmationRequiredOperation
+    {
+        private readonly IEventAggregator eventAggregator;
+        private readonly bool usePassword;
+        private readonly bool uploadPrivateKey;
+
+        private string email;
+        private string password;
+
+        private IdentityTokenRequest request;
+
+        public RegenerateKeyPairOperation(IEventAggregator eventAggregator, bool usePassword, bool uploadPriv
