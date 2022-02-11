4d2f47380b840fc86e8573f746bba2c6b1cb0534
kgenx <112470300+kgenx@users.noreply.github.com>
2022-02-11 07:46:35 +0000

    tidy code

--- Sources/Virgil.Disk/ViewModels/Operations/RegenerateKeyPairOperation.cs
+++ Sources/Virgil.Disk/ViewModels/Operations/RegenerateKeyPairOperation.cs
@@ -18,4 +18,23 @@ namespace Virgil.Sync.ViewModels.Operations
 
         private IdentityTokenRequest request;
 
-        public RegenerateKeyPairOperation(IEventAggregator eventAggregator, bool usePassword, bool uploadPriv
+        public RegenerateKeyPairOperation(IEventAggregator eventAggregator, bool usePassword, bool uploadPrivateKey)
+        {
+            this.eventAggregator = eventAggregator;
+            this.usePassword = usePassword;
+            this.uploadPrivateKey = uploadPrivateKey;
+        }
+
+        public async Task Initiate(string email, string password)
+        {
+            this.email = email.Trim().ToLowerInvariant();
+            this.password = password;
+
+            this.request = await Identity.Verify(this.email);
+        }
+
+        public async Task Confirm(string code)
+        {
+            var token = await this.request.Confirm(code);
+
+            var card = await PersonalCard.Create(token, this.usePassw
