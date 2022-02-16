e4a6c8a8a00c4bdfb0627b0852d30ea2086139c3
kgenx <112470300+kgenx@users.noreply.github.com>
2022-02-16 10:45:55 +0000

    Source code cleanup

---
+++ Sources/Virgil.Disk/WidnowsPerUserEncryptor.cs
@@ -0,0 +1,16 @@
+﻿namespace Infrastructure
+{
+    using System.Security.Cryptography;
+
+    public class WindowsPerUserEncryptor : IEncryptor
+    {
+        static readonly byte[] Entropy = { 91, 83, 7, 36, 1, 15, 123 };
+        
+        public byte[] Encrypt(byte[] data)
+        {
+            return ProtectedData.Protect(data, Entropy, DataProtectionScope.CurrentUser);
+        }
+
+        public byte[] Decrypt(byte[] data)
+        {
+            r
