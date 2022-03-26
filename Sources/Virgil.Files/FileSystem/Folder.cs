88c5dcad7f647e8198cdce365cca560f00c784e8
kgenx <112470300+kgenx@users.noreply.github.com>
2022-03-26 05:42:55 +0000

    add composer file

--- Sources/Virgil.Files/FileSystem/Folder.cs
+++ Sources/Virgil.Files/FileSystem/Folder.cs
@@ -4,3 +4,25 @@ namespace Virgil.DropBox.Client.FileSystem
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using Events;
+
+    public class Folder
+    {
+        private readonly IFileEventListener eventListener;
+
+        public string FolderName { get; }
+        
+        public List<LocalFile> Files { get; } = new List<LocalFile>();
+
+        public string FolderPath { get; }
+
+        public Folder(string folderPath, string folderName, IFileEventListener eventListener)
+        {
+            this.FolderName = folderName;
+            this.eventListener = eventListener;
+            this.FolderPath = folderPath;
+        }
+
+        public void Scan()
+        {
+            var paths = Directory.EnumerateFiles(this.FolderPath, 
