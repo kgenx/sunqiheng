dd198dff026817deb67ac861559322a7713a0f4e
kgenx <112470300+kgenx@users.noreply.github.com>
2022-04-03 15:04:37 +0000

    Remove unnecessary whitespace.

--- Sources/Virgil.Files/FileSystem/FolderLink.cs
+++ Sources/Virgil.Files/FileSystem/FolderLink.cs
@@ -1,4 +1,21 @@
 namespace Virgil.DropBox.Client.FileSystem
 {
     using System;
-    using System.Collections.Co
+    using System.Collections.Concurrent;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Encryption;
+    using Events;
+    using Operations;
+
+    public class FolderLink : IFileEventListener
+    {
+        private readonly EncryptionCredentials credentials;
+        private readonly Folder encryptedFolder;
+        private readonly Folder decryptedFolder;
+        private readonly FolderWatcher encryptedFolderWatcher;
+        private readonly FolderWatcher decryptedFolderWatcher;
+
+        private readonly ConcurrentQueue<Operation> operations = new ConcurrentQueue<Operation>();
+        
+       
