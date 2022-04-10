1c7600fae4d517de61be060693602bf85f12efb7
kgenx <112470300+kgenx@users.noreply.github.com>
2022-04-10 13:02:21 +0000

    version updated

--- Sources/Virgil.Files/FileSystem/FolderWatcher.cs
+++ Sources/Virgil.Files/FileSystem/FolderWatcher.cs
@@ -3,4 +3,22 @@ namespace Virgil.DropBox.Client.FileSystem
     using System;
     using System.IO;
     using System.Linq;
-    using System.Reactive
+    using System.Reactive.Linq;
+
+    public class FolderWatcher
+    {
+        private readonly Folder folder;
+        private readonly FileSystemWatcher fileSystemWatcher;
+
+        public FolderWatcher(Folder folder)
+        {
+            this.folder = folder;
+            this.fileSystemWatcher = new FileSystemWatcher(folder.FolderPath)
+            {
+                IncludeSubdirectories = true,
+                InternalBufferSize = 1024 * 64,
+                NotifyFilter =
+                    NotifyFilters.CreationTime |
+                    NotifyFilters.DirectoryName |
+                    NotifyFilters.FileName |
+                    NotifyFilters.LastWrite |
