073993b86684c60d7468b9e9b9617ffd2d5dae54
kgenx <112470300+kgenx@users.noreply.github.com>
2022-04-17 05:30:39 +0000

    xxx

--- Sources/Virgil.Files/FileSystem/Operations/DecryptFileOperation.cs
+++ Sources/Virgil.Files/FileSystem/Operations/DecryptFileOperation.cs
@@ -1,3 +1,25 @@
 namespace Virgil.DropBox.Client.FileSystem.Operations
 {
- 
+    using System.IO;
+    using System.Threading.Tasks;
+    using Encryption;
+
+    public class DecryptFileOperation : Operation
+    {
+        private readonly string source;
+        private readonly string target;
+        private readonly EncryptionCredentials credentials;
+
+        public DecryptFileOperation(string source, string target, EncryptionCredentials credentials)
+        {
+            this.Title = "Encrypt " + source;
+
+            this.source = source;
+            this.target = target;
+
+            this.credentials = credentials;
+        }
+
+        public override async Task Execute()
+        {
+            if (File.Exists(this.tar
