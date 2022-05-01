af27065ad056c05beb62b539b606ab74c1288903
kgenx <112470300+kgenx@users.noreply.github.com>
2022-05-01 14:14:08 +0000

    gradle

--- Sources/Virgil.FolderLink/Core/Operations/Operation.cs
+++ Sources/Virgil.FolderLink/Core/Operations/Operation.cs
@@ -8,4 +8,26 @@ namespace Virgil.FolderLink.Core.Operations
     {
         private IOperationObserver observer;
         private double progress;
-        public TaskCompletionSource<int> CompletionSource { get; } = new
+        public TaskCompletionSource<int> CompletionSource { get; } = new TaskCompletionSource<int>();
+
+        public string Title { get; protected set; }
+        
+        public bool Completed
+        {
+            get
+            {
+                var status = this.CompletionSource.Task.Status;
+                return status == TaskStatus.RanToCompletion || status == TaskStatus.Faulted;
+            }
+        }
+
+        public abstract Task Execute(CancellationToken cancellationToken);
+        
+        protected async Task Wrap(Task payload)
+        {
+            try
+            {
+                await payload;
+                this.CompletionSource.TrySetResult(0);
+            }
+        
