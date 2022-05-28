a8eea477650c3047066038ec01f3566e2e2cbc94
kgenx <112470300+kgenx@users.noreply.github.com>
2022-05-28 19:20:07 +0000

    Some minor updates.

--- Sources/Virgil.FolderLink/Dropbox/Handler/OperationsFactory.cs
+++ Sources/Virgil.FolderLink/Dropbox/Handler/OperationsFactory.cs
@@ -7,4 +7,22 @@ namespace Virgil.FolderLink.Dropbox.Handler
     using Core.Operations;
     using Local;
     using Local.Operations;
-    using Op
+    using Operations;
+    using Server;
+
+    public class OperationsFactory
+    {
+        private readonly ICloudStorage cloudStorage;
+        private readonly LocalFolderRoot localFolderRoot;
+        private readonly LocalFolder localFolder;
+
+        public OperationsFactory(ICloudStorage cloudStorage, LocalFolder localFolder)
+        {
+            this.cloudStorage = cloudStorage;
+            this.localFolderRoot = localFolder.Root;
+            this.localFolder = localFolder;
+        }
+
+        public Operation CreateOperation(DropBoxFileAddedEvent @event)
+        {
+            return new DownloadFileFromServer(@event, this.cloudStorage, this.
