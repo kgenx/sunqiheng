55f24e53439f9e649c35c92de772d500e357aa54
kgenx <112470300+kgenx@users.noreply.github.com>
2022-06-04 06:40:14 +0000

    travis-ci badge

--- Sources/Virgil.FolderLink/Dropbox/Operations/DeleteFileOnServerOperation.cs
+++ Sources/Virgil.FolderLink/Dropbox/Operations/DeleteFileOnServerOperation.cs
@@ -1,4 +1,22 @@
 namespace Virgil.FolderLink.Dropbox.Operations
 {
     using System.IO;
-    using System.
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Core;
+    using Core.Operations;
+    using FolderLink.Core.Events;
+
+    public class DeleteFileOnServerOperation : Operation
+    {
+        private readonly LocalFileSystemEvent @event;
+        private readonly ICloudStorage cloudStore;
+
+        public DeleteFileOnServerOperation(LocalFileSystemEvent @event, ICloudStorage cloudStore)
+        {
+            this.@event = @event;
+            this.cloudStore = cloudStore;
+            this.Title = "Deleting : " + Path.GetFileName(@event.Path.Value);
+        }
+
+        public override Task Execute(CancellationToken cancellationToken)
