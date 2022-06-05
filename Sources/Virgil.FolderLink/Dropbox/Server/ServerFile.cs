ae09d787d3e93ffc86593ffd695dd683cb836299
kgenx <112470300+kgenx@users.noreply.github.com>
2022-06-05 07:55:27 +0000

    initial-commit

--- Sources/Virgil.FolderLink/Dropbox/Server/ServerFile.cs
+++ Sources/Virgil.FolderLink/Dropbox/Server/ServerFile.cs
@@ -1,1 +1,22 @@
-namespace Virgi
+namespace Virgil.FolderLink.Dropbox.Server
+{
+    using System;
+    using Core;
+    using global::Dropbox.Api.Files;
+
+    public class ServerFile
+    {
+        public ServerPath Path { get; set; }
+        public long Bytes { get; set; }
+        public DateTime ServerModified { get; set; }
+        public DateTime ClientModified { get; set; }
+        
+        public ServerFile(FileMetadata entry)
+        {
+            this.Path = ServerPath.FromServerPath(entry.PathLower);
+            this.Bytes = (long) entry.Size;
+            this.ServerModified = entry.ServerModified;
+            this.ClientModified = entry.ClientModified;
+        }
+
+        public override
