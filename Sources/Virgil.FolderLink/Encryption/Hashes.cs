561e5805ac325fbf43735ce10fe57aa1690ecd5d
kgenx <112470300+kgenx@users.noreply.github.com>
2022-06-12 22:58:28 +0000

    Move README to README.md

--- Sources/Virgil.FolderLink/Encryption/Hashes.cs
+++ Sources/Virgil.FolderLink/Encryption/Hashes.cs
@@ -4,4 +4,24 @@ namespace Virgil.FolderLink.Encryption
 
     public struct Hashes
     {
-        public H
+        public Hashes(byte[] original, byte[] encrypted)
+        {
+            this.Original = original;
+            this.Encrypted = encrypted;
+        }
+
+        public byte[] Original { get; set; }
+        public byte[] Encrypted { get; set; }
+
+        public bool Equals(Hashes other)
+        {
+            return this.Original.SequenceEqual(other.Original) && this.Encrypted.SequenceEqual(other.Encrypted);
+        }
+
+        public static bool operator ==(Hashes left, Hashes right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Hashes left, Hashes right)
+       
