350ce883744f027f97ae6b76e0d4a59052af62d3
kgenx <112470300+kgenx@users.noreply.github.com>
2022-06-26 20:02:12 +0000

    updating packages

--- Sources/Virgil.FolderLink/Local/LocalFolderWatcher.cs
+++ Sources/Virgil.FolderLink/Local/LocalFolderWatcher.cs
@@ -1,3 +1,26 @@
 namespace Virgil.FolderLink.Local
 {
-    using Syste
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Reactive.Linq;
+    using System.Threading.Tasks;
+    
+    using Core;
+    using Dropbox;
+    using SDK.Http;
+
+    public class LocalFolderWatcher : IDisposable
+    {
+        private readonly LocalFolder folder;
+        private readonly FileSystemWatcher fileSystemWatcher;
+        private bool disposed = false;
+        private bool stopped = false;
+
+        public LocalFolderWatcher(LocalFolder folder)
+        {
+            this.folder = folder;
+            this.fileSystemWatcher = new FileSystemWatcher(folder.Root.Value)
+            {
+           
