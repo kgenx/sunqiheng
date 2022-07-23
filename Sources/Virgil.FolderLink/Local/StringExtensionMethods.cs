6f81c2973666b2713e683f47ae04b6acad5b698e
kgenx <112470300+kgenx@users.noreply.github.com>
2022-07-23 16:25:29 +0000

    Addressing comments.

--- Sources/Virgil.FolderLink/Local/StringExtensionMethods.cs
+++ Sources/Virgil.FolderLink/Local/StringExtensionMethods.cs
@@ -2,4 +2,14 @@ namespace Virgil.FolderLink.Local
 {
     public static class StringExtensionMethods
     {
-        public static string ReplaceFirst(this string text, string search, string replace
+        public static string ReplaceFirst(this string text, string search, string replace)
+        {
+            int pos = text.IndexOf(search);
+            if (pos < 0)
+            {
+                return text;
+            }
+            return text.Substring(0, pos) + replace + text.Substring(pos + search.Length);
+        }
+    }
+}
