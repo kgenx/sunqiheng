c3ffa302aa182242f367111e656e35b0409b94f1
kgenx <112470300+kgenx@users.noreply.github.com>
2022-07-30 06:08:10 +0000

    README typo

--- Sources/Virgil.FolderLink/Properties/AssemblyInfo.cs
+++ Sources/Virgil.FolderLink/Properties/AssemblyInfo.cs
@@ -6,4 +6,20 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Virgil.FolderLink")]
-[assembly: AssemblyDescript
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Virgil.FolderLink")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("10188a5a-ff14-4d1e-86ac-0f775d3c7c7e")]
+
+// Version information for an assembly co
