2ec7025871d5d44fd1cb9a5dce6e48c9c0d3d6ed
kgenx <112470300+kgenx@users.noreply.github.com>
2022-09-10 06:32:43 +0000

    Refactoring done. UsedObj is a new class, meant to store the objects used by a particular individual, as well as the connection between individual genes and the fields of those objects they use.

--- Sources/Virgil.SDK.Domain/RecipientCard.cs
+++ Sources/Virgil.SDK.Domain/RecipientCard.cs
@@ -16,4 +16,19 @@
         }
 
         protected RecipientCard(RecipientCard recipientCard)
-    
+        {
+            this.VirgilCardDto = recipientCard.VirgilCardDto;
+            this.Id = recipientCard.Id;
+            this.Identity = recipientCard.Identity;
+            this.PublicKey = recipientCard.PublicKey;
+            this.Hash = recipientCard.Hash;
+            this.CreatedAt = recipientCard.CreatedAt;
+        }
+
+        public RecipientCard(CardModel virgilCardDto)
+        {
+            this.VirgilCardDto = virgilCardDto;
+            this.Id = virgilCardDto.Id;
+            this.Identity = new Identity(virgilCardDto.Identity);
+            this.PublicKey = new PublishedPublicKey(virgilCardDto.PublicKey);
+            this.Hash = virgilCardDto.Has
