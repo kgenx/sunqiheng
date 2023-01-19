37ce5300229d7acc586d0a40393bad4ef6041707
kgenx <112470300+kgenx@users.noreply.github.com>
2023-01-19 15:48:13 +0000

    Warning cleanup

---
+++ Sources/Virgil.Sync.CLI.Windows/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Virgil.Sync.CLI.Windows
+{
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+    using Virgil.CLI.Common.Random;
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            var list = Directory.EnumerateFiles(".").ToList();
+            var boroda = Path.GetFileName(list.Last());
+            var bytes = Encoding.UTF8.GetBytes(boroda);
+
+            var bootstr
