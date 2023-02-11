681dd2d38f3c2cc73d0e0d4b8f499c038f9a5f31
kgenx <112470300+kgenx@users.noreply.github.com>
2023-02-11 16:10:43 +0000

    closes #39

--- Sources/cli/Main.cs
+++ Sources/cli/Main.cs
@@ -1,1 +1,20 @@
-﻿usi
+﻿using AppKit;
+using Security;
+
+namespace cli
+{
+	static class MainClass
+	{
+		static void Main (string[] args)
+		{
+			
+
+			NSApplication.Init ();
+
+			new SecRecord (SecKind.InternetPassword);
+
+
+			NSApplication.Main (args);
+		}
+	}
+}
