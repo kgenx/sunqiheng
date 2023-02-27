2c6bd397c0e1b369138a1a94e2d661cb7551b947
kgenx <112470300+kgenx@users.noreply.github.com>
2023-02-27 16:28:26 +0000

    Design Changes

---
+++ Sources/cli/ViewController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace cli
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Do any additional setup after loading the view.
+		}
+
+		public overri
